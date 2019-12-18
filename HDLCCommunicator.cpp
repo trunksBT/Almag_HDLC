df0d7bd68bb5236c92c99a13ebcf9140989568cc
pkorycin <pkorycin>
2019-12-18 20:18:45 +0100

    Remove setup from the interface

--- HDLCCommunicator.cpp
+++ HDLCCommunicator.cpp
@@ -4,16 +4,6 @@
 
 using namespace convert;
 
-void HDLCCommunicator::setupSend(const std::string& address)
-{
-   LOG(warning) << "Empty communicator";
-}
-
-void HDLCCommunicator::setupReceive(const std::string& address)
-{
-   LOG(warning) << "Empty communicator";
-}
-
 bool HDLCCommunicator::send(const std::string& address, HDLCFrameBodyPtr frame)
 {
    LOG(warning) << "Empty communicator";
