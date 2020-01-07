883c0674d280ea634c631a3dc427741e5a82c635
pkorycin <pkorycin>
2020-01-07 19:40:50 +0100

    Add setup functions to communicator interface

--- IHDLCCommunicator.hpp
+++ IHDLCCommunicator.hpp
@@ -11,6 +11,8 @@
 class IHDLCCommunicator
 {
 public:
+   virtual void setupSend(const std::string& address) = 0;
+   virtual void setupReceive(const std::string& address) = 0;
    virtual bool send(const std::string& address, HDLCFrameBodyPtr frame) = 0;
    virtual HDLCFramePtr receive(const std::string& address) = 0;
    virtual ~IHDLCCommunicator() = default;
